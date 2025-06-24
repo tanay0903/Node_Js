366e0853920e4b5c1bc943b4cf0e9c39755c0095
Tanay Kumar <tanay.kumar@panasonic.aero>
2025-06-24 13:07:11 +0530

    added pages

--- server/index.js
+++ server/index.js
@@ -2,9 +2,16 @@ const http = require('http');
 const fs = require('fs');
 
 const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}: New request received\n`;
+    const log = `${Date.now()}: ${req.url} New request received\n`;
     fs.appendFile('log.txt', log , (err, data) =>{
-        res.end("Hello from the server!");
+        switch(req.url){
+            case '/': res.end("HomePage");
+                break;
+            case '/about': res.end("I am the server created by me");
+                break;
+            default:
+                res.end("404 Not Found");
+        }
     });  
     
  });
