518bda6d4dda619ecbde9205dfebee1df8c79aa7
Tanay Kumar <tanay.kumar@panasonic.aero>
2025-06-24 12:28:02 +0530

    made changes in the server folder (added file handling logging)

--- server/index.js
+++ server/index.js
@@ -2,19 +2,11 @@ const http = require('http');
 const fs = require('fs');
 
 const myServer = http.createServer((req, res) => {
-    const log = `${new Date().toISOString()} - ${req.method} ${req.url}\n`;
-    fs.appendFile('server.log', log, (err) => {
-        switch (req.url){
-            case '/':
-                res.end('Welcome to the server!\n');
-                break;
-            case '/about':
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end('I am Tanay Kumar building a http server with Node.js\n');
-                break;
-            default:
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.end('404 Not Found\n');
-                }
-            });
-        });
+    const log = `${Date.now()}: New request received\n`;
+    fs.appendFile('log.txt', log , (err, data) =>{
+        res.end("Hello from the server!");
+    });  
+    
+ });
+
+ myServer.listen(8000, () => console.log("Server Started !! Running on port 8000")); 
